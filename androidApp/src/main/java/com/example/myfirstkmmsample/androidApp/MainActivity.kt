5b2fbce18a0127887bd446334e387f9c4cc6b587
tanifumiya <fumiyadeveloper@gmail.com>
2020-11-10 01:34:52 +0900

    refactoring

--- androidApp/src/main/java/com/example/myfirstkmmsample/androidApp/MainActivity.kt
+++ androidApp/src/main/java/com/example/myfirstkmmsample/androidApp/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.myfirstkmmsample.androidApp
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myfirstkmmsample.shared.Greeting
 import com.example.myfirstkmmsample.shared.RandomUserApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -14,9 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-
         GlobalScope.launch(Dispatchers.IO) {
             RandomUserApi().getUser {
                 Log.d(TAG, "onCreate: $it")
